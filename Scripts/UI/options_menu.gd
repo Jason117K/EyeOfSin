009c9df556efa42721a94af3f2b8d56b03aeb4de
Jason117K <106029159+Jason117K@users.noreply.github.com>
2025-07-02 16:08:29 -0400

    -pre claude code

--- Scripts/UI/options_menu.gd
+++ Scripts/UI/options_menu.gd
@@ -3,4 +3,5 @@ extends Control
 var startScreen = ("res://Scenes/LevelScenes/StartScreen.tscn")
 
 func _on_back_button_pressed() -> void:
+	print("back button clicked")
 	Global.game_controller.change_scene(startScreen)
