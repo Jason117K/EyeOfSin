f38317a86bcd21c2b8748c8c4e69d431007ad5b0
Jason117K <106029159+Jason117K@users.noreply.github.com>
2025-07-02 16:44:57 -0400

    Update options_menu.gd

--- Scripts/UI/options_menu.gd
+++ Scripts/UI/options_menu.gd
@@ -1,7 +1,21 @@
 extends Control
 
 var startScreen = ("res://Scenes/LevelScenes/StartScreen.tscn")
+var backButton 
 
+func _ready() -> void:
+	print("=== OptionsMenu _ready() Debug ===")
+	print("Master slider exists: ", $CenterContainer/VBoxContainer/masterVolumeSlider != null)
+	print("Music slider exists: ", $CenterContainer/VBoxContainer/musicVolumeSlider != null)
+	print("SFX slider exists: ", $CenterContainer/VBoxContainer/sfxVolumeSlider != null)
+	print("Master slider value: ", $CenterContainer/VBoxContainer/masterVolumeSlider.value if $CenterContainer/VBoxContainer/masterVolumeSlider else "null")
+	print("Available audio buses: ")
+	for i in AudioServer.bus_count:
+		print("  Bus ", i, ": ", AudioServer.get_bus_name(i))
+	backButton = get_node("BackButton")
+	print("Baack Button is ", backButton)
+	backButton.connect("pressed",_on_back_button_pressed)
+	
 func _on_back_button_pressed() -> void:
 	print("back button clicked")
 	Global.game_controller.change_scene(startScreen)
