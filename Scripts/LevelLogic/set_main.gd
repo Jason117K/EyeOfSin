051bdd3e9afa51b0cae3c6d3472603e6ed95d26a
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-04-14 17:16:31 -0400

    mass deletions done

--- Scripts/LevelLogic/set_main.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-var ownerScript = preload("res://Scripts/LevelLogic/Main_Controller.gd")
-
-
-func _ready() -> void:
-	#get_parent().set_script(ownerScript)
-	#get_parent()._ready()
-	pass
