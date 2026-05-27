8c77ec8b6fd1d53ab7767d51f17b93b033a0ddd2
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-05-27 19:26:53 -0400

    about try add 2 more

---
+++ _Entities/SwapAbilities/lucretia_grasp.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+
+
+
+func apply_swap_ability()->void:
+	pass
+
+
+#func undo_swap_ability() -> void:
+	#if is_active:
+		#for zombie : Zombie in Global.get_all_zombies():
+			#if zombie != null:
+				#zombie.undoBloodSlow()
+		#stop()	
+	
+
+	
