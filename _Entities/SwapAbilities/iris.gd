3cecd390865c25502179d0873c33a5d3e87ea7c9
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-05-28 10:57:39 -0400

    testing gaze more

--- _Entities/SwapAbilities/iris.gd
+++ _Entities/SwapAbilities/iris.gd
@@ -11,7 +11,11 @@ func _physics_process(delta: float) -> void:
 	if sample_point > 1.0 or sample_point < 0.0:
 		dir = -dir 
 			
-	var path_direction := path.curve.get_point_position(1) - path.curve.get_point_position(0)
+	#var path_direction := path.curve.get_point_position(1) - path.curve.get_point_position(0)
 	
 	sample_point += (delta/travel_time) * dir
-	position = path.curve.get_point_position(0) + path_direction * position_curve.sample(sample_point)
+	var total_length := path.curve.get_baked_length()
+	var t := position_curve.sample(clampf(sample_point,0.0,1.0))
+	position = path.curve.sample_baked(t * total_length)
+	
+	#position = path.curve.get_point_position(0) + path_direction * position_curve.sample(sample_point)
