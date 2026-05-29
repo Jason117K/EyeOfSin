8a8f2531168fcbed1215e8ee7d807cc016e2c976
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-05-29 18:27:03 -0400

    d

--- _Entities/SwapAbilities/baal_eye.gd
+++ _Entities/SwapAbilities/baal_eye.gd
@@ -7,14 +7,19 @@ var sample_point : float = 0.0
 var dir : int = 1
 var initial_pass_done := false
 var loop_start : float
-
+var loop_end : float
 
 func _ready() -> void:
-	#global_position = Vector2(367,-100)
+	path.curve.add_point( Vector2(370,-15), Vector2.ZERO , Vector2.ZERO,2)
 	var total_length := path.curve.get_baked_length()
+	loop_end = path.curve.get_closest_offset(path.curve.get_point_position(2))  / total_length
+	path.curve.remove_point(2)
+	
+	total_length = path.curve.get_baked_length()
 	var length_to_point2 := path.curve.get_closest_offset(path.curve.get_point_position(1))
 	loop_start = length_to_point2 / total_length
-
+	
+	 
 	
 	loop_start = path.curve.get_closest_offset( path.curve.get_point_position(1)) / path.curve.get_baked_length()
 	print("LOOOOP ", loop_start)
@@ -22,7 +27,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var lower_bound := 0.0 if not initial_pass_done else loop_start
-
+	var upper_bound := 1.0 if not initial_pass_done else loop_end
 	if sample_point > 1.0 or sample_point < lower_bound:
 		dir = -dir
 		if not initial_pass_done and sample_point >= 1.0:
@@ -30,6 +35,6 @@ func _physics_process(delta: float) -> void:
 
 	sample_point += (delta/travel_time) * dir
 	var total_length := path.curve.get_baked_length()
-	var t := position_curve.sample(clampf(sample_point, lower_bound, 1.0))
+	var t := position_curve.sample(clampf(sample_point, lower_bound,upper_bound))
 	position = path.curve.sample_baked(t * total_length)
 	
