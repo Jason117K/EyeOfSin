8a3a28c8ccc0d4a2cd055127fde59cc20cd273e7
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-05-31 12:16:36 -0400

    Syn_Bolt_Ability Done

--- _Entities/SynAbility/syn_lightning_ability_bolt.gd
+++ _Entities/SynAbility/syn_lightning_ability_bolt.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var damage := 100
 @export var aftershock_dmg := 25 
-@export var aftershock_interval := 3
+@export var aftershock_interval := 1.2
 @export var aftershock_duration := 10
 
 @onready var lightning_strike_anim := $AnimatedSprite2D
@@ -15,8 +15,14 @@ extends Area2D
 @onready var lightning_connector_line := $LightningConnectorLine2D
 @onready var lightning_connector_anim := $LightningPivot/LightningConnector
 @onready var lightning_connector_pivot := $LightningPivot
+@onready var shadow_orb := $ShadowOrb
+@onready var default_connector_length : float = lightning_connector_line.points[1].length()
+@onready var lightning_connector_area : Area2D = $LightningPivot/LightningConnectorArea
 
+var grid_pos :Vector2
+var grid_size: int = 32 
 var is_on_green := false 
+var is_lightning_connected := false
 
 func _ready() -> void:
 	lightning_ball_anim.hide()
@@ -25,6 +31,11 @@ func _ready() -> void:
 	
 	aftershock_duration_timer.wait_time = aftershock_duration
 	aftershock_duration_timer.timeout.connect(end_ability)
+	
+	grid_pos = mouse_pos_to_grid(global_position)
+	self.global_position = grid_pos	
+	
+
 	
 	Global.register_lightning_ball(self)
 
@@ -35,6 +46,13 @@ func _ready() -> void:
 		self.set_collision_mask_value(3,false)
 		self.set_collision_mask_value(4,false)
 		self.set_collision_mask_value(5,true)
+		
+		lightning_connector_area.set_collision_mask_value(1,false)
+		lightning_connector_area.set_collision_mask_value(2,false)
+		lightning_connector_area.set_collision_mask_value(3,false)
+		lightning_connector_area.set_collision_mask_value(4,false)
+		lightning_connector_area.set_collision_mask_value(5,true)
+		
 		lightning_anim_current = "green"
 		is_on_green = true 
 	else:
@@ -42,8 +60,17 @@ func _ready() -> void:
 		self.set_collision_mask_value(2,false)
 		self.set_collision_mask_value(3,false)
 		self.set_collision_mask_value(4,true)
+
+		lightning_connector_area.set_collision_mask_value(1,false)
+		lightning_connector_area.set_collision_mask_value(2,false)
+		lightning_connector_area.set_collision_mask_value(3,false)
+		lightning_connector_area.set_collision_mask_value(4,true)
+		
 		lightning_anim_current = "purple"
 		is_on_green = false 
+	
+	lightning_connector_anim.animation = lightning_anim_current
+	shadow_orb.animation = lightning_anim_current
 	
 	shock_dmg_timer.timeout.connect(aftershock_damage)
 	shock_dmg_timer.wait_time = aftershock_interval
@@ -75,22 +102,41 @@ func spawn_aftershock()->void:
 	shock_dmg_timer.start()
 
 func aftershock_damage()->void:
+	print("Aftershock Damage Called")
 	for zombie in get_overlapping_areas():
 		if zombie.is_in_group("Zombie"):
 			zombie.take_damage(aftershock_dmg)
-	
+	if is_lightning_connected:
+		for zombie in lightning_connector_area.get_overlapping_areas():
+			if zombie.is_in_group("Zombie"):
+				zombie.take_damage(aftershock_dmg)
 	
 func connect_lightning(lightning_to_connect : Area2D)->void:
 	print("Attempt Connect Lightning")
-	lightning_connector_line.show()
+	lightning_connector_line.hide()
 	var target_local :Vector2 = lightning_connector_line.to_local(lightning_to_connect.global_position)
 	if lightning_connector_line.get_point_count() < 2:
 		lightning_connector_line.add_point(target_local)
 	else:
 		lightning_connector_line.set_point_position(1, target_local)
-		
-	lightning_connector_pivot.global_rotation =  lightning_connector_line.global_position.angle_to_point(lightning_to_connect.global_position)
+	
+	var angle = lightning_connector_pivot.global_position.angle_to_point(lightning_to_connect.global_position)
+	lightning_connector_pivot.global_rotation = angle - PI / 2.0
+	
+	var distance = lightning_connector_pivot.global_position.distance_to(lightning_to_connect.global_position)
+	lightning_connector_pivot.scale.y = distance / default_connector_length
+	
+	place_shadow_orb(lightning_to_connect.global_position)
+	
 	lightning_connector_anim.play()
+	
+	is_lightning_connected = true 
+
+func place_shadow_orb(new_position:Vector2)->void:
+	shadow_orb.global_position = new_position
+	shadow_orb.show()
+	shadow_orb.play()
+
 		
 func end_ability()->void:
 	Global.deregister_lightning_ball(self)
@@ -100,6 +146,9 @@ func end_ability()->void:
 	
 	
 	
+	
+func mouse_pos_to_grid(mouse_pos: Vector2) -> Vector2:
+	return Vector2(floor(mouse_pos.x / grid_size), floor(mouse_pos.y / grid_size)) * grid_size
 	
 	
 	
