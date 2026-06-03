203f637db0bb890c09e250640b2a2a6821262369
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-06-03 17:03:10 -0400

    about to fix ability

--- _Stages/Level4/level_0_4.gd
+++ _Stages/Level4/level_0_4.gd
@@ -96,18 +96,18 @@ func _configure_waves() -> void:
 											{ "Severed": 9}])
 	zombie_spawner_2.set_waves_from_dicts([{},
 											{"Severed" : 3}, 
-											{"Severed": 2, "Reborn": 9}, 
+											{"Severed": 2, "Reborn": 6}, 
 											{"Severed": 2, "Unhallower": 1, "Reanimator" : 1}])
 	zombie_spawner_3.set_waves_from_dicts([{"Reborn":6,"Severed":2},
-											{"Severed": 6},
+											{"Severed": 5},
 											{"Severed": 3, "Unhallower":1}, 
 											{"Reborn": 6, "Unhallower": 3, "Reanimator":2}])
-	zombie_spawner_4.set_waves_from_dicts([{"Severed": 5}, 
-											{"Severed": 3, "Reborn":6},
+	zombie_spawner_4.set_waves_from_dicts([{"Severed": 4}, 
+											{"Severed": 3, "Reborn":5},
 											{"Reborn": 4, "Severed": 1}, 
 											{"Severed": 6, "Unhallower":2}]) 
-	zombie_spawner_5.set_waves_from_dicts([{"Reborn":6,"Severed":3}, 
-											{"Severed": 3, "Reborn":7, "Unhallower":1},
+	zombie_spawner_5.set_waves_from_dicts([{"Reborn":5,"Severed":2}, 
+											{"Severed": 2, "Reborn":6, "Unhallower":1},
 											{"Reborn": 4, "Severed": 1, "Reanimator":1}, 
 											{"Reborn": 9, "Reanimator":2}])
 	zombie_spawner_6.set_waves_from_dicts([{}, 
