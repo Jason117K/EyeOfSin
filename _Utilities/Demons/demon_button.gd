6b5bcf5ed7b5dfd8efc5826c04e1d8a76755aae6
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-06-05 14:59:17 -0400

    l

--- _Utilities/Demons/demon_button.gd
+++ _Utilities/Demons/demon_button.gd
@@ -1,4 +1,4 @@
-extends TextureRect
+extends TextureButton
 @onready var mat: ShaderMaterial = self.material
 
 func _on_mouse_entered()->void: set_hover(1.0)
@@ -7,6 +7,9 @@ func _on_mouse_exited()->void:  set_hover(0.0)
 func set_hover(target:float)->void:
 	var current :float = mat.get_shader_parameter("hovering")
 	mat.set_shader_parameter("hovering", lerp(current, target, 0.2))
+	
+	
+	
 func _process(_delta:float)->void:
 	RenderingServer.global_shader_parameter_set(
 		"mouse_screen_pos", get_global_mouse_position()
