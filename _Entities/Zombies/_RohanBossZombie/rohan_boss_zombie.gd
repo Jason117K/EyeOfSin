1fec5d5facc15c1eabd06ab122ccb7b6097c1a06
Jason117K <106029159+Jason117K@users.noreply.github.com>
2026-06-22 17:24:21 -0400

    working on new rohan

--- _Entities/Zombies/_RohanBossZombie/rohan_boss_zombie.gd
+++ _Entities/Zombies/_RohanBossZombie/rohan_boss_zombie.gd
@@ -1,21 +1,23 @@
 extends Zombie
-# BucketHeadZombie.gd
+# rohan_boss_zombie.gd
 
 func _ready()->void:
 	super()
 	if Global.gameIsStarted && self.is_demo == false:
-		Global.unlock_zombie("Unhallower")
+		Global.unlock_zombie("Rohan")
 
 func get_zombie_name() -> String:
-	return " UNHALLOWER "
+	return " ROHAN "
 
 
 func silence() -> void:
 	super()
 	attackComp.silence()
 
+#Temporary Will Replace
 func get_special_description() -> String:
 	return unhallower_special_description
-
+	
+#Temporary Will Replace
 func get_zombie_icon() -> CompressedTexture2D:
 	return Global.unhallower_icon
